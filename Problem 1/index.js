ea50509a4d8957c08fac8f45346bb4d9922211f6
anitasfyun <anitasfyunn@gmail.com>
2023-10-04 12:38:26 +0700

    pengumpulan_tugas_w7

--- Problem 1/index.js
+++ Problem 1/index.js
@@ -1,6 +1,9 @@
 var arr = [];
 function createObj(name, phase, gender) {
   // YOUR CODE HERE...
+  var student = {name, phase, gender};
+  arr.push(student);
+
 
   // END OF YOUR CODE HERE...
   return arr;
@@ -17,7 +20,9 @@ console.log(arr);
 function getData(name) {
   let result;
   // YOUR CODE HERE...
-
+  result = arr.find(function (student) {
+    return student.name === name;
+  });
   // END OF YOUR CODE HERE...
   return result;
 }
